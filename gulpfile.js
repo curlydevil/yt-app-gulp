4c5d1ad958122bc912a027705edcf70798c96d1a
d.berzoy <d.berzoy@gmail.com>
2015-08-13 13:22:12 +0300

    Revert "modified to cleanup all temp mess"
    This reverts commit ae0d52b68aff1f79001d573ab03ab2c696f4edc9.

--- gulpfile.js
+++ gulpfile.js
@@ -9,28 +9,17 @@ gulp.task('help', $.taskListing);
 
 gulp.task('default', ['help']);
 
-gulp.task('cleanAll', cleanAllTask);
+gulp.task('clean', cleanTask);
 
-gulp.task('copyFonts', ['getSource'], copyFontsTask);
+gulp.task('copyFonts', copyFontsTask);
 
-gulp.task('getSource', ['cleanAll'], getSourceTask);
+gulp.task('getSource', ['clean'], getSourceTask);
 
 gulp.task('templatecache', ['getSource'], templateCacheTask);
 
 gulp.task('minify', ['templatecache', 'copyFonts'], minifyTask);
 
-gulp.task('process', ['minify'], cleanTempTask);
 ///
-
-var tempFiles = [
-        config.tmp,
-        config.root + 'index.html',
-        config.root + 'favicon.ico',
-        config.root + 'web.config',
-        config.root + 'README.md',
-        config.root + 'vendor/',
-        config.root + 'src/'
-    ];
 
 function copyFontsTask() {
     log('Copying fonts');
@@ -90,15 +79,18 @@ function minifyTask() {
         .pipe(gulp.dest(config.build));
 }
 
-function cleanAllTask(done) {
-    log('cleaning all files');
-
-    clean(tempFiles.concat(config.build), done);
-}
-
-function cleanTempTask(done) {
-    log('cleaning all temp files');
-    clean(tempFiles, done);
+function cleanTask(done) {
+    log('Cleaning build folder');
+    var files = [
+        config.build,
+        config.tmp,
+        config.root + 'index.html',
+        config.root + 'favicon.ico',
+        config.root + 'web.config',
+        config.root + 'vendor/',
+        config.root + 'src/'
+    ];
+    clean(files, done);
 }
 
 function getSourceTask(done) {
