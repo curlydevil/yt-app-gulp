0c178d037e10b4346e1a942a21f05a3660ae480e
d.berzoy <d.berzoy@gmail.com>
2015-08-13 15:15:01 +0300

    moved back a little further

---
+++ README.md
@@ -0,0 +1,4 @@
+This project's purpose was to test our knowledge in web-dev. Particularly in AngularJS and its $resource object integration with YouTube's REST API.
+
+Live version of this webapp you can see here:
+http://yt-app.azurewebsites.net
--- gulpfile.js
+++ gulpfile.js
@@ -9,49 +9,17 @@ gulp.task('help', $.taskListing);
 
 gulp.task('default', ['help']);
 
-gulp.task('cleanAll', ['azureCleanUp'], cleanAllTask);
-
-gulp.task('azureCleanUp', azureCleanUpTask);
+gulp.task('clean', cleanTask);
 
 gulp.task('copyFonts', ['getSource'], copyFontsTask);
 
-gulp.task('getSource', ['cleanAll'], getSourceTask);
+gulp.task('getSource', ['clean'], getSourceTask);
 
 gulp.task('templatecache', ['getSource'], templateCacheTask);
 
 gulp.task('minify', ['templatecache', 'copyFonts'], minifyTask);
 
-gulp.task('process', ['minify'], buildCleanUpTask);
-
-gulp.task('azure', ['process'], azureTask);
 ///
-
-var tempFiles = [
-        config.tmp,
-        config.root + 'index.html',
-        config.root + 'favicon.ico',
-        config.root + 'web.config',
-        config.root + 'README.md',
-        config.root + 'vendor/',
-        config.root + 'src/'
-    ];
-
-function azureTask(done) {
-    return gulp
-        .src(config.build + '**/*.*')
-        .pipe(gulp.dest(config.root));
-}
-
-function azureCleanUpTask(done) {
-    var files = [
-        config.root + 'fonts',
-        config.root + 'js',
-        config.root + 'styles',
-        config.root + 'index.html'
-    ];
-
-    clean(files, done);
-}
 
 function copyFontsTask() {
     log('Copying fonts');
@@ -111,15 +79,19 @@ function minifyTask() {
         .pipe(gulp.dest(config.build));
 }
 
-function cleanAllTask(done) {
-    log('cleaning all files');
-
-    clean(tempFiles.concat(config.build), done);
-}
-
-function buildCleanUpTask(done) {
-    log('cleaning all temp files');
-    clean(tempFiles, done);
+function cleanTask(done) {
+    log('Cleaning build folder');
+    var files = [
+        config.build,
+        config.tmp,
+        config.root + 'index.html',
+        config.root + 'favicon.ico',
+        config.root + 'web.config',
+        config.root + 'README.md',
+        config.root + 'vendor/',
+        config.root + 'src/'
+    ];
+    clean(files, done);
 }
 
 function getSourceTask(done) {
